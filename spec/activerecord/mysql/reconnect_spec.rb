ba305cf2a57d6f2adf23bd960a74ea30b6d3d436
Genki Sugawara <sugawara@cookpad.com>
2014-01-03 18:33:20 +0900

    fix spec

--- spec/activerecord/mysql/reconnect_spec.rb
+++ spec/activerecord/mysql/reconnect_spec.rb
@@ -1,4 +1,12 @@
 describe Hash do
+  it 'select all' do
+    expect {
+      expect(Employee.all.length).to eq(300024)
+      mysql_restart
+      expect(Employee.all.length).to eq(300024)
+    }.to_not raise_error
+  end
+
   it 'count' do
     expect(Employee.count).to eq(300024)
     mysql_restart
