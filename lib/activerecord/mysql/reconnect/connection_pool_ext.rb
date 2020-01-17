9be23414ee9e65b209dd84f183bf86e5db72e3bf
Prem Sichanugrist <s@sikac.hu>
2020-01-17 18:11:30 +0900

    Load database config from db_config instead

--- lib/activerecord/mysql/reconnect/connection_pool_ext.rb
+++ lib/activerecord/mysql/reconnect/connection_pool_ext.rb
@@ -1,9 +1,16 @@
 module Activerecord::Mysql::Reconnect::NewConnectionWithRetry
   def new_connection
-    Activerecord::Mysql::Reconnect.retryable(
-      :proc => proc { super },
-      :connection => spec.config,
-    )
+    if defined?(db_config)
+      Activerecord::Mysql::Reconnect.retryable(
+        :proc => proc { super },
+        :connection => db_config.configuration_hash
+      )
+    else
+      Activerecord::Mysql::Reconnect.retryable(
+        :proc => proc { super },
+        :connection => spec.config,
+      )
+    end
   end
 end
 class ActiveRecord::ConnectionAdapters::ConnectionPool
